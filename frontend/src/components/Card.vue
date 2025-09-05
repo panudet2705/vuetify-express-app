591a9d7d9ac413c7ed3b9267c0da4ed581bb1718
panudet2705 <panudetsaengwiset@gmail.com>
2025-09-05 14:56:33 +0700

    this is my first deployment website for me

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { onMounted, ref } from 'vue'
+  import api from '@/service/api'
+
+  const message = ref('')
+
+  onMounted(async () => {
+    const res = await api.get('/api/hello')
+    message.value = res.data.message
+  })
+</script>
+
+<template>
+  <v-container>
+    <v-card class="pa-4">
+      <h2>{{ message }}</h2>
+    </v-card>
+  </v-container>
+</template>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <div>
+      <h2>This is my Test build server</h2>
+    </div>
+
+    <Card />
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
